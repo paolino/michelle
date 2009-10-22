f6d5fcc2dde51620790246975f6a20c88e7bbf23
paolino <paolino@paolinoub.(none)>
2009-10-22 15:39:47 +0200

    making Dump

--- Dump.hs
+++ Dump.hs
@@ -15,11 +15,11 @@ import Data.List
 import Debug.Trace
 import Data.Tree.Zipper as Z 
 import Data.Tree
-import Lib (mapAccumM, dup'TChan, contents)
+import Lib (mapAccumM, dup'TChan, contents, prune)
 
 dump :: Store -> STM ([Ctx],[Restoring])
 dump t = do
-	let ys = flatten . tree $ t
+	let ys = flatten . tree $ pruneM (\(Load (SMrt ts _) _ _ _) -> readTVar ts >>= return . isNothing) t
 	fmap unzip . forM ys $ \(Load (SMrt ts _) ctx tc evs) -> do
 		s <- readTVar ts
 		c <- readTVar tc
@@ -27,30 +27,3 @@ dump t = do
 		return (ctx,(SMs s,ejs))
 
 
-onAugmented :: (a -> b) -> (b -> a) -> (TreeLoc b -> Maybe (TreeLoc b)) -> TreeLoc a -> Maybe (TreeLoc a) 
-onAugmented fab fba f t = fmap (fromTree . fmap fba . tree) . f . fromTree . fmap fab . tree $ t
-
-process :: Show a => (a -> Bool) -> TreeLoc (Bool,a) -> Maybe (TreeLoc (Bool,a))
-process f t = let 	(x,y) = getLabel t 
-			k = 	if isLeaf t then 
-					if f y then deleteRight t 
-					else  Z.right t `mplus` parent t
-				else 	if not x then firstChild . setLabel (True,y) $ t 
-					else  Z.right t `mplus` parent t
-			in (k >>= process f) `mplus` Just t
-
-
-deleteRight :: Show a => TreeLoc a -> Maybe (TreeLoc a)
-deleteRight loc = case rights loc of
-	[] -> modifyTree (\t -> t { subForest = lefts loc }) `fmap` parent loc 
-	t : ts -> Just loc { tree = t, rights = ts }
-
-prune :: Show a => (a -> Bool) -> TreeLoc a -> Maybe (TreeLoc a) 
-prune f = onAugmented ((,) False) snd (process f)
-
-test n m = let 
-	t = fromTree $ unfoldTree (\x -> if x < n then (x + 1, [x * 2, x * 3]) else (x + 1,[])) 1 
-	Just t' = prune (> m) t
-	in do
-		putStr . drawTree . fmap show . tree $ t
-		putStr . drawTree . fmap show . tree $ t' 
--- Lib.hs
+++ Lib.hs
@@ -4,6 +4,8 @@ import Data.Traversable as T
 import Control.Monad.State
 import Data.Tree.Zipper
 import Data.Tree
+import Debug.Trace
+import Control.Applicative
 
 
 
@@ -35,27 +37,42 @@ contents t = do
 onAugmented :: (a -> b) -> (b -> a) -> (TreeLoc b -> Maybe (TreeLoc b)) -> TreeLoc a -> Maybe (TreeLoc a) 
 onAugmented fab fba f t = fmap (fromTree . fmap fba . tree) . f . fromTree . fmap fab . tree $ t
 
-process :: Show a => (a -> Bool) -> TreeLoc (Bool,a) -> Maybe (TreeLoc (Bool,a))
-process f t = let 	(x,y) = getLabel t 
-			k = 	if isLeaf t then 
-					if f y then deleteRight t 
-					else  right t `mplus` parent t
-				else 	if not x then firstChild . setLabel (True,y) $ t 
-					else  right t `mplus` parent t
-			in (k >>= process f) `mplus` Just t
+onAugmentedM :: (Monad m, Functor m) => (a -> b) -> (b -> a) -> (TreeLoc b -> m (Maybe (TreeLoc b))) -> TreeLoc a -> m (Maybe (TreeLoc a)) 
+onAugmentedM fab fba f t = fmap (fmap (fromTree . fmap fba . tree)) .  f . fromTree . fmap fab . tree $ t
 
+process :: (a -> Bool) -> TreeLoc (Bool,a) -> Maybe (TreeLoc (Bool,a))
+process f t = 	let 	(x,y) = getLabel t 
+			run = (right t `mplus` parent t >>= process f) `mplus` Just t 
+		in 	if isLeaf t then 
+				if f y then deleteRight t >>= process f
+				else run
+			else 	if not x then (firstChild . setLabel (True,y) $ t) >>= process f
+				else run
 
-deleteRight :: Show a => TreeLoc a -> Maybe (TreeLoc a)
+processM :: Monad m => (a -> m Bool) -> TreeLoc (Bool,a) -> m (Maybe (TreeLoc (Bool,a)))
+processM f t = do
+	let 	(x,y) = getLabel t 
+		run = case right t `mplus` parent t of 
+			Nothing -> return $ Just t
+			Just t' -> processM f t' 
+	if isLeaf t then do
+			g <- f y
+			if g then case deleteRight t of
+				Nothing -> return Nothing
+				Just t' -> processM f t'
+				else run
+		else 	if not x then case firstChild . setLabel (True,y) $ t of
+				Nothing -> return Nothing
+				Just t' -> processM f t' 
+				else run
+
+deleteRight :: TreeLoc a -> Maybe (TreeLoc a)
 deleteRight loc = case rights loc of
 	[] -> modifyTree (\t -> t { subForest = lefts loc }) `fmap` parent loc 
 	t : ts -> Just loc { tree = t, rights = ts }
 
-prune :: Show a => (a -> Bool) -> TreeLoc a -> Maybe (TreeLoc a) 
+prune :: (a -> Bool) -> TreeLoc a -> Maybe (TreeLoc a) 
 prune f = onAugmented ((,) False) snd (process f)
 
-test n m = let 
-	t = fromTree $ unfoldTree (\x -> if x < n then (x + 1, [x * 2, x * 3]) else (x + 1,[])) 1 
-	Just t' = prune (> m) t
-	in do
-		putStr . drawTree . fmap show . tree $ t
-		putStr . drawTree . fmap show . tree $ t'
+pruneM :: (Monad m, Functor m) => (a -> m Bool) -> TreeLoc a -> m (Maybe (TreeLoc a)) 
+pruneM f = onAugmentedM ((,) False) snd (processM f)
